48f39214893380da1ce57221bf080c4860171c0f
Robert Herber <rwherber@gmail.com>
2020-01-30 21:41:54 -0800

    chore: add azure urls to gateway

--- gateway/gateway/index.ts
+++ gateway/gateway/index.ts
@@ -3,8 +3,10 @@ import { ApolloGateway } from '@apollo/gateway';
 
 const gateway = new ApolloGateway({
     serviceList: [
-        { name: 'products', url: 'http://localhost:7072/products' },
-        { name: 'reviews', url: 'http://localhost:7071/reviews' },
+        // { name: 'products', url: 'http://localhost:7072/products' },
+        // { name: 'reviews', url: 'http://localhost:7071/reviews' },
+        { name: 'products', url: 'https://rherber-gql-products.azurewebsites.net/products' },
+        { name: 'reviews', url: 'https://rherber-gql-reviews.azurewebsites.net/reviews' },
     ],
 });
 
